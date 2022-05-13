5b75f56279b94df6573cf75415f8c74982081096
agvan <agvandyke25@gmail.com>
2022-05-13 13:42:21 -0500

    created the buttonEl variable to target the button in the html w/ the #save-task id & identifies it as a DOM element & helps to keep track of which variables store DOM elements & added multi-line comment

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+/* Targets the <button> element in HTML. The button element is now named buttonEl.
+The used of camelCase marks the element as a JavaScript variable.
+The El suffix indentifies this as a DOM element & helps to keep track of which variables store DOM elements*/
