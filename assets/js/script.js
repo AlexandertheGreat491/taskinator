a4a40e1859b7558a6fb95882079dc3b0de26b7b4
agvan <agvandyke25@gmail.com>
2022-05-13 14:30:23 -0500

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-/* Targets the <button> element in HTML. The button element is now named buttonEl.
-The used of camelCase marks the element as a JavaScript variable.
-The El suffix indentifies this as a DOM element & helps to keep track of which variables store DOM elements*/
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
