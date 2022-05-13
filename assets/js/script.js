b95f787182e2afc64fb1ba22bfcaaaf5c31044df
agvan <agvandyke25@gmail.com>
2022-05-13 17:58:01 -0500

    added comments to explain the use of the querySelector()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
+// document.querySelector returns the first element within the document that mat hes the spcified selector//
+//document.querySelector("#save-task") targets the <button> element from index.html
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-
+/*document.querySelector("#tasks-to-do") targets the <ul> element in the <ul> under
+the <main class="page-content" in the <section class="task-list-wrapper" under the
+<h2>
+*/
 var createTaskHandler = function() {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
