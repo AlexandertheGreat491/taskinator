ad4b46cec0b41ba697135b29e1b21fba454e3555
agvan <agvandyke25@gmail.com>
2022-05-14 16:26:27 -0500

    updated the taskStatusChangeHandler()

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,9 +180,18 @@ var taskButtonHandler = function(event){
         deleteTask(taskId);
     }
 };
+
 var taskStatusChangeHandler = function(event) {
-    
-}
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".taskitem[data-task-id='" + taskId + "']");
+};
+
 var editTask = function(taskId) {
     //console.log("editing task #" + taskId);
     //get task list item element
