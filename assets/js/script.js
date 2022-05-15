0bd6f9a3b98cb71486f1f3682d5385afefd21394
agvan <agvandyke25@gmail.com>
2022-05-15 12:06:56 -0500

    added for loop under deleteTask() and empty updatedTaskArr

--- assets/js/script.js
+++ assets/js/script.js
@@ -215,6 +215,19 @@ var deleteTask = function(taskId) {
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
   //remove() method removes the element from the DOM.
+  // create new array to hold updated list of tasks
+  varupdatedTaskArr = [];
+
+  // loop through current tasks
+  for (var i = 0; i < tasks.length; i++) {
+    // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+    if (tasks[i].id !== parseInt(taskId)) {
+      updatedTaskArr.push(tasks[i]);
+    }
+  }
+  // keeps the tasks array up-to-date at all times
+  // reassign tasks array to be the same as updatedTaskArr
+  tasks = updatedTaskArr;
 };
 
 // Create a new task
